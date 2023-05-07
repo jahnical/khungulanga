f77b11f9907d975e1b26cba1463e1729b8af1cda
Jahnics <jahnics1@gmail.com>
2023-05-07 18:46:09 +0200

    diagnosis

--- smartskin/urls.py
+++ smartskin/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.authtoken import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls', namespace='api')),
     path('api/api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

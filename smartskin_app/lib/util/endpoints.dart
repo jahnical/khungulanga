a88b064b8671b8c696d642511780703dc79d4f9e
Jahnics <jahnics1@gmail.com>
2023-05-10 13:30:11 +0200

    resolved the assemble stuck error

--- smartskin_app/lib/util/endpoints.dart
+++
@@ -1,8 +0,0 @@
-
-const BASE_URL = "http://192.168.42.74:8000/";
-const API_BASE_URL = "${BASE_URL}api";
-const TOKEN_URL = "$API_BASE_URL/api-token-auth";
-const REGISTER_URL = "$API_BASE_URL/users/register";
-const DIAGNOSIS_URL = "$API_BASE_URL/diagnosis";
-
-const DERMATOLOGISTS_URL = "$API_BASE_URL/dermatologists";

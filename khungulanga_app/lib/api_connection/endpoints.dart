04fbd644b0387c3a1fec01bc81469e310764ca6b
Jahnics <jahnics1@gmail.com>
2023-06-07 21:07:49 +0200

    host

--- khungulanga_app/lib/api_connection/endpoints.dart
+++ khungulanga_app/lib/api_connection/endpoints.dart
@@ -1,5 +1,5 @@
 
-const BASE_URL = "https://4d86-105-234-160-35.ngrok-free.app/";
+const BASE_URL = "https://ict7.pythonanywhere.com/";
 const API_BASE_URL = "${BASE_URL}api";
 const TOKEN_URL = "$API_BASE_URL/api-token-auth/";
 const REGISTER_URL = "$API_BASE_URL/users/register/";

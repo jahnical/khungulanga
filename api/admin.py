5124256f040954552481e6d53016527bc763c828
Jahnics <jahnics1@gmail.com>
2023-06-21 10:58:35 +0200

    admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from api.models.appointment import Appointment
+from api.models.clinic import Clinic
+from api.models.dermatologist import Dermatologist
+from api.models.diagnosis import Diagnosis
+from api.models.disease import Disease
+from api.models.notification import Notification
+from api.models.patient import Patient
+from api.models.prediction import Prediction
+from api.models.slot import Slot
+from api.models.treatment import Treatment
+
 # Register your models here.
+admin.site.site_header = 'Khungulanga Admin'
+admin.site.register([Patient, Dermatologist, Clinic, Disease, Prediction, Treatment, Appointment, Slot, Diagnosis, Notification])
--- api/views/prediction.py
+++ api/views/prediction.py
@@ -38,10 +38,13 @@ class PredictionAPIView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         serializer = PredictionSerializer(prediction, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return Response(serializer.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data)
+        
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, *args, **kwargs):
         try:

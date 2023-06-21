f2236f148326bf9590b4857f91054c8a33bcf601
Jahnics <jahnics1@gmail.com>
2023-06-21 16:04:16 +0200

    no disease

--- api/skindisease/identify.py
+++ api/skindisease/identify.py
@@ -29,7 +29,7 @@ models = {
 labels = {
     "Face": {'acne vulgaris': 0, 'basal cell carcinoma': 1, 'rosacea': 2, 'squamous cell carcinoma': 3, 'urticaria': 4},
     "Upper Body": {'acne vulgaris': 0, 'basal cell carcinoma': 1, 'folliculitis': 2, 'lichen planus': 3, 'melanoma': 4, 'psoriasis': 5, 'rosacea': 6, 'squamous cell carcinoma': 7},
-    "Legs Feet": {'basal cell carcinoma': 0, 'folliculitis': 1, 'melanoma': 2, 'psoriasis': 3, 'scabies': 4, 'squamous cell carcinoma': 5},
+    "Legs Feet": {'basal cell carcinoma': 0, 'folliculitis': 1, 'melanoma': 2, 'psoriasis': 3, 'scabies': 4, 'squamous cell carcinoma': 5, 'zed': 6},
     "Arms Hands": {'actinic keratosis': 0, 'allergic contact dermatitis': 1, 'basal cell carcinoma': 2, 'lichen planus': 3, 'psoriasis': 4, 'scabies': 5, 'urticaria': 6}
 }
 
@@ -38,6 +38,11 @@ diseases = Disease.objects.all()
 def map_predictions(diagnosis, predictions, labels):
     disease_prob = [{"disease": disease, "probability": predictions[labels[disease]]} for disease in labels]
     disease_prob.sort(key=lambda x: x["probability"], reverse=True)
+    
+    if disease_prob[0]['disease'] == 'zed' and disease_prob[0]['probability'] > 0.5:
+        return []
+    
+    disease_prob = list(filter(lambda x: not (x["disease"] == 'zed'), disease_prob))
     
     return [Prediction.objects.create(
         disease=diseases.get(name=disease["disease"]), 
